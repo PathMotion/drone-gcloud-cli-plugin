12c519c1bee505cf88deb2d7f0e00a8efdfd907c
Maxime Lebastard <maxime@pathmotion.com>
2019-01-30 17:03:18 +0100

    Fix repository url in push

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push maximelebastard/drone-gcloud-cli-plugin
+docker push pathmotion/drone-gcloud-cli-plugin
